a0be653341af2300d9ff48db24864c90fd642e3f
keptan <keptan@protonmail.com>
2018-11-18 21:58:59 -1000

    files

---
+++ mioSays.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+icon=$(ls icons/ | sort -R | head -n 1);
+bubble=$(ls bubbles/ | sort -R | head -n 1);
+IFS=';' read -ra sizeArgs <<< "$bubble"
+
+downFromTitle=74
+boxSize=${sizeArgs[0]}
+boxPos=${sizeArgs[1]}
+iconPos=${sizeArgs[2]}
+iconWidth=$( identify -ping -format "%w" icons/$icon);
+mouthDown=$(( $downFromTitle+$iconPos-$icon))
+
+convert -background transparent -fill black -font FreeSerif -size ${sizeArgs[0]} -gravity Center caption:@- miff:- |\
+convert bubbles/$bubble - -gravity NorthWest -compose Over -geometry ${sizeArgs[1]} -composite miff:- |\
+convert -background transparent -page -300-0 title.png -page +0+$downFromTitle - -page -$iconWidth+$mouthDown icons/$icon -layers merge $1
